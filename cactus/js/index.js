28ea6adf12fe8f69e86781a5cf0819e6db7d0283
wera <69810414+natlujan@users.noreply.github.com>
2023-08-22 18:29:45 -0700

    Update index.js

--- cactus/js/index.js
+++ cactus/js/index.js
@@ -20,7 +20,18 @@ function calcularSuma2(){
     n1 = $("#txtN1").val();
     n2 = $("#txtN2").val();
 
+    if (n1 == "0" || n2 == "0")
+    {
+        swal.fire(
+            "Error",
+            "Debe capturar números > 0 para N1 y N2",
+            "error"
+        );
+    }
+    else{
+
     res = parseInt(n1) + parseInt(n2)
 
     $("#txtRes").val(res)
+    }
 }
